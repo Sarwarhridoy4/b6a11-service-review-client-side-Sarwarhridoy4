2f567a2ced9687813d44e2f426a299097dfea59a
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-08 09:08:35 +0600

    basic route setup done

---
+++ src/Pages/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">This Is Services Route</h2>
+        </div>
+    );
+};
+
+export default Services;
--- src/Router/Routes/Routes.jsx
+++ src/Router/Routes/Routes.jsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Blogs from "../../Pages/Blogs/Blogs";
+import Services from "../../Pages/Services/Services";
 
 // Router Setup
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/services",
+        element: <Services></Services>,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+    ],
   },
 ]);
 
