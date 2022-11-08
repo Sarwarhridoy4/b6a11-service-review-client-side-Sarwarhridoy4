053a84e133560eef99623283e7a35319903fa970
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-08 20:37:24 +0600

    leaflet map added in home page but not working

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../../../Banner/Banner";
 import Clients from "../../Clients/Clients";
+import Locate from "../../Locate/Locate";
 import Services from "../../Services/Services";
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner></Banner>
       <Services></Services>
       <Clients></Clients>
+      <Locate></Locate>
     </div>
   );
 };
---
+++ src/Pages/Locate/Locate.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import "./Locate.css"
+const Locate = () => {
+  return (
+    <div>
+      <h2 className='text-4xl font-semibold'>Locate Me:</h2>
+      <div id="map" className='map-cntainer w-1/2 h-2/5 mx-auto'>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Locate;
