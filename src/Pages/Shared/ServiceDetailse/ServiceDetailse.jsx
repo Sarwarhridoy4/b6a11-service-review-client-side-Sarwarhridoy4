1a75a82e401ebf422cb8d28136deeafb6d03e484
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-09 17:29:54 +0600

    react-image-view also impimented on service detail page

--- src/Pages/Shared/ServiceDetailse/ServiceDetailse.jsx
+++ src/Pages/Shared/ServiceDetailse/ServiceDetailse.jsx
@@ -1,4 +1,6 @@
+import { PhotoProvider, PhotoView } from "react-photo-view";
 import { useLoaderData } from "react-router-dom";
+import 'react-photo-view/dist/react-photo-view.css';
 
 
 const ServiceDetailse = () => {
@@ -9,10 +11,17 @@ const ServiceDetailse = () => {
     <div>
       <div className='mockup-window border bg-sky-300 m-20'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5 px-4 py-16 bg-base-200'>
+        <PhotoProvider
+        speed={() => 800}
+        easing={(type) => (type === 2 ? 'cubic-bezier(0.36, 0, 0.66, -0.56)' : 'cubic-bezier(0.34, 1.56, 0.64, 1)')}
+        >
           <div className="image flex flex-col gap-4 justify-center items-center">
-            <img className="" src={image} alt={title} />
+          <PhotoView src={image}>
+              <img className="" src={image} alt={title} />
+              </PhotoView>
             <h3 className="text-2xl text-sky-500 uppercase">price:💲{price}</h3>
-          </div>
+            </div>
+            </PhotoProvider>
           <div className="description">
             <h2 className="text-3xl text-sky-600 my-5">Service Description</h2>
             <p className="text-justify">{description}</p>
