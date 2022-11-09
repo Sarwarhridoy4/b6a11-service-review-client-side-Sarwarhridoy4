7c630ad2e65f47121813bb2ed15371d5e0445183
Sarwarhridoy4 <sarwarhridoy4@gmail.com>
2022-11-09 22:42:51 +0600

    signup form changed

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FcGoogle } from 'react-icons/fc';
+import { Link } from "react-router-dom";
 
 
 const LogIn = () => {
@@ -10,6 +11,7 @@ const LogIn = () => {
           <div className='text-center lg:text-left'>
             <h1 className='text-5xl font-bold text-center'>Login now!</h1>
           </div>
+          <p>New Here?<Link to='/signup'>Register Now</Link></p>
           <div className='card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100'>
             <div className='card-body'>
               <div className='form-control'>
@@ -39,7 +41,7 @@ const LogIn = () => {
               </div>
               <div className='form-control mt-6'>
                 <button className='btn btn-primary'>Login</button>
-                <button className='btn btn-primary'>
+                <button className='btn btn-primary mt-4'>
                <FcGoogle style={{'width':'30px'}}></FcGoogle>
                   Login</button>
                 
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,135 +1,54 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { AuthContext } from "../../contexts/Auth/AuthProvider";
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-  const {createUser} = useContext(AuthContext)
-  const handelSignup = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    // const name = form.name.value;
-    const email = form.email.value;
-    const password = form.password.value;
-    // const ImageURL = form.ImageURL.value;
-    console.log(email,password);
-
-    createUser( email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-      })
-    .catch(error=> console.error(error));
-  };
   return (
     <div>
-      <form
-        onSubmit={handelSignup}
-        className='w-full max-w-md p-4 rounded-md shadow sm:p-8 bg-gray-900 text-gray-100 mx-auto my-10'
-      >
-        <h2 className='mb-3 text-3xl font-semibold text-center'>
-          Sign Up Now!
-        </h2>
-        <p className='text-sm text-center text-gray-400'>
-          Already An User?
-          <Link to='/login' className='focus:underline hover:underline'>
-            Log here
-          </Link>
-        </p>
-        <div className='my-6 space-y-4'>
-          <button
-            aria-label='Login with Google'
-            type='button'
-            className='flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 border-gray-400 focus:ring-violet-400'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 32 32'
-              className='w-5 h-5 fill-current'
-            >
-              <path d='M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z'></path>
-            </svg>
-            <p>Login with Google</p>
-          </button>
-        </div>
-        <div className='flex items-center w-full my-4'>
-          <hr className='w-full text-gray-400' />
-          <p className='px-3 text-gray-400'>OR</p>
-          <hr className='w-full text-gray-400' />
-        </div>
-        <form className='space-y-8 ng-untouched ng-pristine ng-valid'>
-          <div className='space-y-4'>
-            <div className='space-y-2'>
-              <label htmlFor='name' className='block text-sm'>
-                Enter Your Name
-              </label>
-              <input
-                type='text'
-                name='name'
-                
-                placeholder='name'
-                className='w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400'
-                required
-              />
-            </div>
-            <div className='space-y-2'>
-              <label htmlFor='email' className='block text-sm'>
-                Email address
-              </label>
-              <input
-                type='email'
-                name='email'
-               
-                placeholder='email'
-                className='w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400'
-                required
-              />
-            </div>
-            <div className='space-y-2'>
-              <div className='flex justify-between'>
-                <label htmlFor='password' className='text-sm'>
-                  Password
+      <div className='hero min-h-screen bg-base-200'>
+        <div className='hero-content flex-col'>
+          <div className='text-center lg:text-left'>
+            <h1 className='text-5xl font-bold text-center'>Signup Here</h1>
+          </div>
+          <p>Have an Account Already?<Link to='/login'>Sign In Now</Link></p>
+          <div className='card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100'>
+            <div className='card-body'>
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Email</span>
                 </label>
-                <a
-                  rel='noopener noreferrer'
-                  href='/'
-                  className='text-xs hover:underline text-gray-400'
-                >
-                  Forgot password?
-                </a>
+                <input
+                  type='text'
+                  placeholder='email'
+                  className='input input-bordered'
+                />
               </div>
-              <input
-                type='password'
-                name='password'
-               
-                placeholder='password'
-                className='w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400'
-                required
-              />
-            </div>
-            <div className='space-y-2'>
-              <div className='flex justify-between'>
-                <label htmlFor='ImageURL' className='text-sm'>
-                  ImageURL
+              <div className='form-control'>
+                <label className='label'>
+                  <span className='label-text'>Password</span>
+                </label>
+                <input
+                  type='text'
+                  placeholder='password'
+                  className='input input-bordered'
+                />
+                <label className='label'>
+                  <a href='/' className='label-text-alt link link-hover'>
+                    Forgot password?
+                  </a>
                 </label>
               </div>
-              <input
-                type='text'
-                name='ImageURL'
+              <div className='form-control mt-6'>
+                <button className='btn btn-primary'>Login</button>
+                <button className='btn btn-primary mt-4'>
+               <FcGoogle style={{'width':'30px'}}></FcGoogle>
+                  Login</button>
                 
-                placeholder='Provide image URL'
-                required
-                className='w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100 focus:border-violet-400'
-              />
+              </div>
             </div>
           </div>
-          <button
-            type='button'
-            className='w-full px-8 py-3 font-semibold rounded-md bg-violet-400 text-gray-900'
-          >
-            Sign in
-          </button>
-        </form>
-      </form>
+        </div>
+      </div>
     </div>
   );
 };
