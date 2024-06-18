6d2fb4e41b2f86e44d08270eda0aa367e40044ea
Sarwar Hossain <sarwarhridoy4@gmail.com>
2024-06-18 18:32:31 +0600

    fix:map not showing

--- src/Pages/Locate/Locate.jsx
+++ src/Pages/Locate/Locate.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "./Locate.css"
+import "leaflet/dist/leaflet.css";
+import "./Locate.css";
+
 const Locate = () => {
+  const position = [23.8761, 90.3798]; // Coordinates for Azampur, Uttara, Dhaka
+
   return (
     <div>
       <h2 className='text-4xl font-semibold'>Locate Me:</h2>
-      <div id="map" className='map-cntainer w-1/2 h-2/5 mx-auto'>
+      <div id='map' className='map-container w-1/2 h-2/5 mx-auto'>
         <MapContainer
-          center={[51.505, -0.09]}
+          center={position}
           zoom={13}
           scrollWheelZoom={false}
+          style={{ height: "400px", width: "100%" }} // Ensure the map has a height
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           />
-          <Marker position={[51.505, -0.09]}>
+          <Marker position={position}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
