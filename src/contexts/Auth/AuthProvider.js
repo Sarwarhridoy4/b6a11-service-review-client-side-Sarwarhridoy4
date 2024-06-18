f1c575edaeafad814e913f65e521d0ec8b3a59df
Sarwar Hossain <sarwarhridoy4@gmail.com>
2024-06-18 17:54:31 +0600

    remove clg

--- src/contexts/Auth/AuthProvider.js
+++ src/contexts/Auth/AuthProvider.js
@@ -48,7 +48,7 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      console.log(currentUser);
+      // console.log(currentUser);
      setUser(currentUser);
      setLoading(false)
    });
